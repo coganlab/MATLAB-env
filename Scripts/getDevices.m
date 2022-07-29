caf75e0da9086899d387e7752e4db7a199e96986
coganlab <83311118+coganlab@users.noreply.github.com>
2022-07-29 17:44:56 -0400

    adding task script functions

---
+++ Scripts/getDevices.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb8bd1fc9957ae4be0513b01c74d313599039b1232755f1c8fc3f6ab6cdc2fa0
+size 634
---
+++ Scripts/pause_script.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1774c8203077ed4e85b13c12c21d2b2d947625cfd7f348b46e159a0e5a62773a
+size 838
