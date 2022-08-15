74a39d0bd6ddee52f971052977cb3b9535e9d894
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-08-15 17:57:08 -0400

    fixed pause_script to work with any laptop model

--- Scripts/pause_script.m
+++ Scripts/pause_script.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1774c8203077ed4e85b13c12c21d2b2d947625cfd7f348b46e159a0e5a62773a
-size 838
+oid sha256:73d0f3d931fff3f93e15ce4b6099835615a1ba6ba8ac7ca154f2b43b3eb32274
+size 937
