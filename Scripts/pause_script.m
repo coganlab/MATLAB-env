8862c0b20c97080f5b80c8e5442d8d7e7e7156c3
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-08-15 19:04:43 -0400

    fixed text and exit key in pause_script

--- Scripts/pause_script.m
+++ Scripts/pause_script.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b14ca49f27a6e9872e08fb3172d7df578dc069d90477b4d9a764163d46f83c99
-size 941
+oid sha256:52ac4eaf5180dc9561ef90ccd5b61fafb8a7a7e84fe2f6cfe50c7de746da2963
+size 928
