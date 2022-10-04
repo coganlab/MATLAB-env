eeee2c3b7bd238d05a7c6e8fb1ea28c55bb8c7f3
Aaronearlerichardson <jakdaxter31@gmail.com>
2022-10-04 09:54:57 -0400

    made poptasksubjectdata cross-compatible

--- Scripts/Populate/popTaskSubjectData.m
+++ Scripts/Populate/popTaskSubjectData.m
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3a129b8deb57f2c670dbd57045f99f0cfe6a6123a8cb3693acd812941d6cbc4
-size 8926
+oid sha256:979fb72f7b4c9d657c16ae1cc75deb601ee20819e637d3fe7c2cd457c44d4bc1
+size 8935
