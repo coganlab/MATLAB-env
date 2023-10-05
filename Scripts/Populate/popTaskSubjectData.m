9c11896e28577feffa010ecdde08643fd1b5449f
dsuseendar@gmail.com <47862632+dsuseendar@users.noreply.github.com>
2023-10-05 15:32:55 -0400

    Updated populate scripts to assign white-matter contacts with grey-matter ROIs based on the radius and voxel percentage

--- Scripts/Populate/popTaskSubjectData.m
+++ Scripts/Populate/popTaskSubjectData.m
@@ -1,4 +1,19 @@
-function Subject = popTaskSubjectData(Task)
+function Subject = popTaskSubjectData(Task, options)
+% Populates subject data for a given task, including channel and ROI information.
+%
+% Args:
+%   Task (struct): Task information.
+%   options (struct, optional): Options for the data population process.
+%       - gmThresh (double): Grey matter threshold for assigning channels (default: 0.05).
+%       - voxRadius (double): Voxel radius for ROI analysis (default: 10).
+%
+% Returns:
+%   Subject (struct array): Populated subject data.
+arguments
+    Task
+    options.gmThresh = 0.05;
+    options.voxRadius = 10;
+end
 
 % make the box path global and make sure its set
 global BOX_DIR
@@ -8,8 +23,6 @@ global RECONDIR
 if ~isfield(Task,'Directory')
     Task.('Directory')=fullfile(BOX_DIR, 'CoganLab', 'D_Data', Task.Name);
 end
-
-
 
 
 TASK_DIR=Task.Directory;
@@ -118,7 +131,7 @@ for iF=1:length(fileDir)
     if exist(muscleChannelsPath, 'file')
         
         muscleChannels = load(muscleChannelsPath);
-        muscleChannels.muscleChannel
+        muscleChannels.muscleChannel;
         Subject(iF).badChannels = unique([Subject(iF).badChannels find(muscleChannels.muscleIds)']);
     end
     
@@ -139,13 +152,9 @@ end
 
 
 % this will populate the channel information (name, location)
-
-counterChan=0;
-iElecS=0;
-%SNList=1:length(Subject);
 load([RECONDIR filesep 'missingRecons.mat']);
 missRecIdx=ones(length(Subject),1);
-counter=0;
+
 for iSN=1:length(Subject)
     for iM=1:length(missingRecons)
         if strcmp(Subject(iSN).Name,['D' num2str(missingRecons(iM))]);
@@ -156,114 +165,47 @@ end
 
 SNList=find(missRecIdx==1);
 
-for iSN=1:length(SNList)
-    clear elecIdx
+subjRoiInfo = extractRoiInformation(Subject(SNList),voxRad=options.voxRadius);
+
+chanInfo = assignRoiInformation(subjRoiInfo, gmThresh=options.gmThresh);
+
+for iSN = 1:length(SNList)
+    
     SN=SNList(iSN);
-    load(fullfile(TASK_DIR, Subject(SN).Name, 'mat','experiment.mat'))
-    rfiledir=[RECONDIR filesep Subject(SN).Name filesep 'elec_recon' filesep];
-    if ~isfile([ rfiledir filesep Subject(SN).Name '_elec_locations_RAS.txt' ])
-        warning(['Skipping subject ' Subject(SN).Name '\n%s Reason: ' ...
-            'missing required files in ' rfiledir ])
-        continue
-    end
-    elecs=list_electrodes(Subject(SN).Name);
-    
-    % load regular locations if seeg, brain shifted if grid
-    if strcmp(Subject(SN).Type,'SEEG')  %|| strcmp(Subject(SN).Name,'D16')
-        filename=[rfiledir Subject(SN).Name '_elec_location_radius_3mm_aparc+aseg.mgz.csv'];
-        % filename=[rfiledir Subject(SN).Name '_elec_location_radius_3mm_aparc.a2009s+aseg.mgz.csv'];
-        elecInfo = parse_RAS_file([rfiledir Subject(SN).Name '_elec_locations_RAS.txt']);
-    elseif strcmp(Subject(SN).Type,'ECoG')
-        filename=[rfiledir Subject(SN).Name '_elec_location_radius_3mm_aparc+aseg.mgz_brainshifted.csv'];
-        %   filename=[rfiledir Subject(SN).Name '_elec_location_radius_3mm_aparc.a2009s+aseg.mgz_brainshifted.csv'];
-        elecInfo = parse_RAS_file([rfiledir Subject(SN).Name '_elec_locations_RAS_brainshifted.txt']);
+    Subject(SN).Name
+    chanInfoSubj = chanInfo(iSN).ChannelInfo;
+    chanNameRoi = {chanInfoSubj.Name};
+   % load(fullfile(TASK_DIR, Subject(SN).Name, 'mat','experiment.mat'))
+    chanNameActual = strcat(Subject(SN).Name, '-', {Subject(SN).Experiment.channels.name});
+    length(chanNameRoi)
+    length(chanNameActual)
+    chanInfoSubjAligned = [];
+    wmId = [];
+    for iChan = 1:length(chanNameActual)
         
+        idTrue = ismember(chanNameRoi,chanNameActual(iChan));
+        sum(idTrue)
+        if(sum(idTrue))
+            chanInfoSubjChan = chanInfoSubj(idTrue);
+            chanInfoSubjAligned(iChan).Name=chanInfoSubjChan.Name;
+            chanInfoSubjAligned(iChan).xyz=chanInfoSubjChan.xyz;
+            chanInfoSubjAligned(iChan).Location=chanInfoSubjChan.Location;
+            if(contains(chanInfoSubjAligned(iChan).Location,["White","hypointensities"]))
+                wmId = [wmId iChan];
+            end
+        else
+            continue
+        end
     end
     
-    % read through the xls file
-    %[NUM,TXT,RAW]=xlsread(filename);
-    T=readtable(filename);
-    TXT=T;
-    NUM=table2array(T(:,[4,6,8,10]));
-%     NUM=zeros(size(T,1),4);
-%     iN_List=[4,6,8,10];
-%     for iN=1:5
-%         NUM(:,iN)=table2array(T(:,iN_List(iN)));
-%     end
-%     NUM=table2cell(T(:,[4,6,8,10]));
+    Subject(SN).ChannelInfo = chanInfoSubjAligned;
+    %wmIds = contains({chanInfoSubjAligned.Location},["White","hypointensities"]);
+    Subject(SN).WM = wmId;
+    Subject(SN).Task=Task.Name;    
+end
 
-    %TXT=TXT(2:end,:);
-    Tname=table2array(TXT(:,2));
-    Trest=table2cell(TXT(:,1:2:size(TXT,2)));
-    
-    for iElec=1:length(elecs);
-        for iElec2=1:size(TXT,1)
-            if strcmp(elecs{iElec},(strcat(Subject(SN).Name,'-',Tname(iElec2,:))))
-                elecIdx(iElec)=iElec2;
-            end
-        end
-    end
-    ii=find(elecIdx==0);
-    elecIdx(ii)=[];
-    TXT=TXT(elecIdx,:);
-    NUM=NUM(elecIdx,:);
-    Tname=Tname(elecIdx,:);
-    Trest=Trest(elecIdx,:);
-    % this can be adjusted! Looks for proportion that is white matter (less
-    % than 0.67) and keeps it. Could be higher or lower
-    for iElec=1:size(TXT,1)
-        elec_nameT=strcat(Subject(SN).Name, '-',Tname(iElec,:));
-        iN=1;
-        
-        while (contains(Trest(iElec,iN),'White-Matter'))  && iN<=7 && NUM(iElec,iN)<0.8
-            %|| NUM(iElec,iN-2)<0.5
-            iN=iN+1;
-        end
-        
-%         while (contains(Trest(iElec,iN),'Unknown') || contains(Trest(iElec,iN),'unknown') || contains(Trest(iElec,iN),'hypointensities')   ) && NUM(iElec,iN)==1
-%         subj_labels_loc(iElecS+iElec)='Unknown';
-%         end
-        
-        while (contains(Trest(iElec,iN),'Unknown') || contains(Trest(iElec,iN),'unknown') || contains(Trest(iElec,iN),'hypointensities')   ) && NUM(iElec,iN)<1 && iN<=7
-            %|| NUM(iElec,iN-2)<0.5
-            iN=iN+1;
-        end
 
-        subj_labels2(iElecS+iElec)=elec_nameT;
-        %
-        subj_labels_loc(iElecS+iElec)=Trest(iElec,iN); % percentages
-        %    subj_labels_loc{iElecS+iElec}=TXT(iElec+1,1); % first column
-    end
-    
-    % writes labels and names to Subject file
-    for iElec=1:length(experiment.channels)
-        for iElec2=1:length(subj_labels2)
-            if strcmp(subj_labels2{iElec2},[Subject(SN).Name '-' experiment.channels(iElec).name])
-                Subject(SN).ChannelInfo(iElec).Name=subj_labels2{iElec2};
-                Subject(SN).ChannelInfo(iElec).Location=subj_labels_loc{iElec2};
-            end
-        end
-    end
-    
-    % locate white matter + no label electrodes?
-    
-    counter=0;
-    for iChan=1:length(Subject(SN).ChannelInfo);
-        if isempty(Subject(SN).ChannelInfo(iChan).Location) || contains(Subject(SN).ChannelInfo(iChan).Location, 'White-Matter') || contains(Subject(SN).ChannelInfo(iChan).Location, ' ')
-            Subject(SN).WM(counter+1)=iChan;
-            counter=counter+1;
-        end
-    end
-    
-    for iElec1=1:length(Subject(iSN).ChannelInfo)
-        for iElec2=1:length(elecInfo.labels)
-            if strcmp(Subject(iSN).ChannelInfo(iElec1).Name,[Subject(iSN).Name '-' elecInfo.labels{iElec2}])
-                Subject(iSN).ChannelInfo(iElec1).xyz=elecInfo.xyz(iElec2,:);
-            end
-        end
-    end
-    
-    Subject(iSN).Task=Task.Name;
+   
 end
 
 
