e0844783311301ea77afea66bdfe94fcbda10f23
dsuseendar@gmail.com <47862632+dsuseendar@users.noreply.github.com>
2024-04-17 10:53:00 -0400

    Script to run electrode localization stats for different parcellation techniques

--- recon_code/preprocessing_tools/run_elec_location_stats.m
+++ recon_code/preprocessing_tools/run_elec_location_stats.m
@@ -3,11 +3,12 @@ function run_elec_location_stats(subj,options)
 arguments
     subj
     options.mm = [1 3 5 7 9 10]
+    options.parcs = {'aparc.BN_atlas+aseg.mgz','aparc.a2009s+aseg.mgz','aparc+aseg.mgz'};
 end
 
 etype = get_elec_type(subj);
 
-parcs = {'aparc+aseg.mgz', 'aparc.a2009s+aseg.mgz'};
+parcs = options.parcs;
 mm = options.mm;
 for p = 1:numel(parcs)
     for m = 1:numel(mm)
